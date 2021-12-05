78c34a972aa5c7cb68ca91b15f4683e0b3e020cf
adiamb <ambati@stanford.edu>
2021-12-05 12:04:43 -0800

    d2p2

--- scripts/d2.py
+++ scripts/d2.py
@@ -35,3 +35,37 @@ def part1():
         print('The Ans to Part1 is {}'.format(h*d))
 
 part1()
+
+class submarineAim(submarine):
+    def __init__(self, object):
+        super().__init__(object)
+        self.aim = object[4]
+
+    def updown(self):
+        if self.command == 'up':
+            self.aim -= self.step
+        else:
+            self.aim += self.step
+        return self.horiz, self.depth, self.aim
+
+    def fwd(self):
+        self.horiz += self.step
+        self.depth += (self.aim*self.step)
+        return self.horiz, self.depth, self.aim
+
+
+def part2():
+    with open('inputs/d2.txt', 'r') as dirFile:
+        h, d, aim = 0, 0, 0
+        for line in dirFile:
+            row = line.strip().split(' ')
+            dirTup = (row[0], int(row[1]), h, d, aim)
+            drive=submarineAim(dirTup)
+            if row[0] == 'forward':
+                h, d, aim = drive.fwd()
+            else:
+                h, d, aim = drive.updown()
+            #print('H {} DEP {} AIM {} line {}'.format(h, d, aim, line))
+        print('The Ans to Part2 is {}'.format(h*d))
+
+part2()
