a945c450728d8553c938203238c4ed03766b5509
adiamb <ambati@stanford.edu>
2021-12-04 21:41:08 -0800

    d1

--- scripts/d1.py
+++ scripts/d1.py
@@ -1,11 +1,20 @@
-def getdepth(input):
-    stream=[int(i.strip()) for i in open(input, 'r')]# as inFile:
+def part1(stream):
     n = 0
     for i in range(0, len(stream)-1):
-        if stream[i+1] >= stream[i]:
+        if stream[i+1] > stream[i]:
             n +=1
     print(n)
     return(n)
 
-getdepth(input='inputs/d1p1.txt')
+stream=[int(i.strip()) for i in open('inputs/d1p1.txt', 'r')]
+part1(stream)
 
+def part2(stream):
+    stream2=[]
+    for i in range(0, len(stream), 1):
+        temp = stream[i:i+3]
+        if len(temp) == 3:
+            stream2 +=[sum(temp)]
+    part1(stream2)
+
+part2(stream)
